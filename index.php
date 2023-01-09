bba4f5eddcff7dc49d620d685aa27b79ac2293bc
VincenzoZ24 <zitov287@gmail.com>
2023-01-09 16:30:45 +0100

    completato

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    class movie
+    {
+        public $titolo;
+        public $durata;
+        public $genere;
+
+        public function __construct($titolo, $durata, $genere)
+        {
+            $this->titolo = $titolo;
+            $this->setdurata($durata);
+            $this->genere = $genere;
+        }
+
+
+        public function setdurata($durata)
+        {
+            if (is_integer($durata) && $durata >= 0) {
+                $this->durata = $durata;
+            }
+            return $durata;
+        }
+    }
+
+    $film1 = new movie('Spiderman', 180 , 'Animazione');
+    $film2 = new movie('Top Gun', 200 , 'Azione');
+    var_dump($film1);
+    var_dump($film2);
+
+    ?>
+</body>
+
+</html>
